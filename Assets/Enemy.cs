dab52f8f4101d47770c0025f760e673f0d80ed6f
Gryfs <Allan18040@gmail.com>
2020-10-16 12:38:42 +0200

    4

---
+++ Assets/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class Enemy : MonoBehaviour
+{
+    public GameObject player;
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
