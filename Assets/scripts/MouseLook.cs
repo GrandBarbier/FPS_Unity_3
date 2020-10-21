26915619c8a19123f4d7f3c2c08a746f7ef096d1
Lelio <lelio.rollin@e-artsup.net>
2020-10-21 22:51:10 +0200

    Input Manager fix

--- Assets/scripts/MouseLook.cs
+++ Assets/scripts/MouseLook.cs
@@ -18,7 +18,7 @@ public class MouseLook : MonoBehaviour
     void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * -mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -80f, 90f);
