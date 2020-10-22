2b7269dd972f293e07b4a4533aa9841a5b12ea01
Gryfs <Allan18040@gmail.com>
2020-10-22 12:54:49 +0200

    Map 1 fini

---
+++ Assets/scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class Enemy : MonoBehaviour
+{
+    public GameObject player;
+    private NavMeshAgent agent;
+
+    public int health = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void TakeDamage()
+    {
+        health = health - 1;
+
+        
+    }
+
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
