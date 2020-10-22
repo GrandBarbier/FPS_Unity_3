2af4b80d9fb27a9b23718f68f0305b55bb8c63f5
Narrako <deneve.cynthia14@gmail.com>
2020-10-22 13:29:44 +0200

    spawnEnemy

---
+++ Assets/scripts/EnemySpawner/LeelManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting; 
+
+public class LeelManager : MonoBehaviour
+{
+    public Wave[] allWave;
+    public int maxWaveCount;
+    public Transform spawnPosition;
+    
+    [System.Serializable] 
+    public class Wave
+    {
+        public EnemySpawner[] spawners;
+    }
+
+    public void Start()
+    {
+        StartCoroutine(SpawnEnemyWave(maxWaveCount));
+    }
+    
+    public IEnumerator SpawnEnemyWave(int maxWave)
+    {
+        if (maxWave > allWave.Length) maxWave = allWave.Length;
+        
+        // Loop in all needed wave
+        for (int i = 0; i < maxWave; i++)
+        {
+            List<GameObject> enemies = new List<GameObject>();
+            
+            // Spawn 1 wave
+            foreach (var spawner in allWave[i].spawners)
+            {
+                enemies.Add(spawner.Spawn());
+            }
+            
+            // Check all ennemies are dead
+            while (enemies.Count > 0)
+            {
+                for (int y = 0; y < enemies.Count; y++)
+                {
+                    if (!enemies[y])
+                    {
+                        enemies.RemoveAt(y);
+                        y--;
+                    }
+                }
+                
+                yield return new WaitForSeconds(Time.deltaTime);
+            }
+        }
+        yield return null;
+    }
+}
